320219e08de8c5752b5fa7e619779793a20da4ef
DongvinPark <dongvin99@naver.com>
2024-12-02 23:08:53 +0900

    Logger 클래스 구현

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,19 @@
 #include <boost/asio.hpp>
 #include <iostream>
 
-#include "dto/Res.h"
+#include "util/Logger.h"
 
 using boost::asio::ip::tcp;
 
 int main() {
-    try {
-        boost::asio::io_service io_service;
-        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 8554));
-
-        std::cout << "Server is running on port 8554..." << std::endl;
-
-        while (true) {
-            tcp::socket socket(io_service);
-            acceptor.accept(socket);
-	        std::cout << "client socket connected!!\n";
-
-            Res resObject{ 9, "DongvinPark" };
-            std::string message = "Server sent Object to Client >> key : "
-            + std::to_string(resObject.getKey())
-            + ", val : " + resObject.getVal() + "\n";
-
-            std::cout << "sent message : " << message << "\n";
-
-            boost::system::error_code ignored_error;
-            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
-            std::cout << "wrote response!!\n";
-        }
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-
+    
+    auto logger = Logger::getLogger("main");
+    logger->severe("This is a severe message.");
+    logger->warning("This is a warning message.");
+    logger->info("This is an info message.");
+    logger->debug("This is a debug message.");
+    logger->setUserInputPrompt(false);
+    logger->info("Input prompt disabled.");
     return 0;
+
 }
---
+++ src/util/Logger.cpp
@@ -0,0 +1,52 @@
+#include "Logger.h"
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <sstream>
+
+Logger::Logger(const std::string& className)
+    : className(className), showPrompt(true) {}
+
+std::shared_ptr<Logger> Logger::getLogger(const std::string& className) {
+    return std::shared_ptr<Logger>(new Logger(className));
+}
+
+void Logger::setUserInputPrompt(bool prompt) {
+    showPrompt = prompt;
+}
+
+void Logger::severe(const std::string& msg) {
+    print(makeString(msg, "SEVERE", ANSI_RED));
+}
+
+void Logger::warning(const std::string& msg) {
+    print(makeString(msg, "WARNING", ANSI_YELLOW));
+}
+
+void Logger::info(const std::string& msg) {
+    print(makeString(msg, "INFO", ANSI_WHITE));
+}
+
+void Logger::debug(const std::string& msg) {
+    print(makeString(msg, "DEBUG", ANSI_GREEN));
+}
+
+std::string Logger::makeString(const std::string& msg, const std::string& level, const std::string& color) {
+    auto now = std::chrono::system_clock::now();
+    auto time_t_now = std::chrono::system_clock::to_time_t(now);
+    auto local_time = *std::localtime(&time_t_now);
+
+    std::ostringstream oss;
+    oss << color
+        << std::put_time(&local_time, "%Y-%m-%d %H:%M:%S")
+        << " [" << level << "] " << className << ": " << msg
+        << ANSI_RESET;
+    return oss.str();
+}
+
+void Logger::print(const std::string& msg) {
+    std::cout << msg << std::endl;
+    if (showPrompt) {
+        std::cout << ">> ";
+    }
+}
