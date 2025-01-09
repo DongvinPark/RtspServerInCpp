f9dbdb03eee605b4e7c09328e7971ccccece17ca
DongvinPark <dongvin99@naver.com>
2025-01-09 22:02:40 +0900

    init RtspHandler's member functions

--- include/RtspHandler.h
+++ include/RtspHandler.h
@@ -20,6 +20,47 @@ public:
   std::unique_ptr<Buffer> handleRtspRequest(std::string reqStr);
 
 private:
+  bool hasSessionId(std::vector<std::string> strings);
+  void respondOptions(Buffer& buffer);
+  void respondDescribe(Buffer& buffer, std::string mediaInfo, std::string content);
+  void respondSetup(
+    Buffer& buffer, std::string transport, std::string sessionId, int64_t ssrc,
+    int trackId, int refVideoSampleCnt, int camDirectoryCnt
+  );
+  void respondSetupForHybrid(Buffer& buffer, std::string sessionId, std::string hybridMode);
+  void respondPlay(
+    Buffer& buffer, std::vector<int> seq, std::vector<int64_t> rtpTime,
+    std::vector<std::string> urls, std::string sessionId
+  );
+  void respondPlayAfterPause(std::string sessionId);
+  void respondSwitching(std::string sessionId);
+  void respondCameraChange(std::string sessionId);
+  void respondBitrateChange(std::string sessionId);
+  void respondTeardown();
+  void respondError(int error, std::string rtspMethod);
+  void respondPause(Buffer& buffer);
+
+  std::string findUserName(std::vector<std::string> strings);
+  int findCSeq(std::vector<std::string> strings);
+  std::string findContents(std::string line0);
+  std::string getMediaInfo(std::string fullCid);
+  int findTrackId(std::string line0);
+  std::string findTransport(std::vector<std::string> strings);
+  std::string findHybridMode(std::vector<std::string> strings);
+  std::string findNotTx(std::vector<std::string> strings);
+  std::vector<int> findChannels(std::string transport);
+  std::string findSessionId(std::vector<std::string> strings);
+  std::vector<float> findNormalPlayTime(std::vector<std::string> strings);
+  std::string findDeviceModelName(std::vector<std::string> strings);
+  std::string findManufacturer(std::vector<std::string> strings);
+  int findLatestReceivecSampleIdx(std::vector<std::string> strings, std::string filter);
+  bool isSeekRequest(std::vector<std::string> strings);
+  bool isValidPlayTime(std::vector<float> ntpSec);
+  std::string getContentsTitle(std::vector<std::string> urls);
+  std::string getSupportingBitrateTypes(std::vector<int> bitrateTypes);
+  bool isContainingPlayInfoHeader(std::vector<std::string> strings);
+  bool isThereMonitoringInfoHeader(std::vector<std::string> strings);
+  void parseHybridVideoSampleMetaDataForDandS(std::string notTxIdListStr);
 
   std::shared_ptr<Logger> logger;
   std::weak_ptr<Session> parentSessionPtr;
--- src/service/RtspHandler.cpp
+++ src/service/RtspHandler.cpp
@@ -27,4 +27,136 @@ RtspHandler::RtspHandler(
     sessionId(inputSessionId) {}
 
 RtspHandler::~RtspHandler() {
+
+}
+
+void RtspHandler::shutdown() {
+}
+
+std::unique_ptr<Buffer> RtspHandler::run(std::unique_ptr<Buffer> inputBufferPtr) {
+}
+
+std::unique_ptr<Buffer> RtspHandler::handleRtspRequest(std::string reqStr) {
+}
+
+bool RtspHandler::hasSessionId(std::vector<std::string> strings) {
+}
+
+void RtspHandler::respondOptions(Buffer &buffer) {
+}
+
+void RtspHandler::respondDescribe(Buffer &buffer, std::string mediaInfo, std::string content) {
+}
+
+void RtspHandler::respondSetup(
+  Buffer &buffer,
+  std::string transport,
+  std::string sessionId,
+  int64_t ssrc,
+  int trackId,
+  int refVideoSampleCnt,
+  int camDirectoryCnt
+) {
+}
+
+void RtspHandler::respondSetupForHybrid(
+  Buffer &buffer,
+  std::string sessionId,
+  std::string hybridMode
+) {
+}
+
+void RtspHandler::respondPlay(
+  Buffer &buffer,
+  std::vector<int> seq,
+  std::vector<int64_t> rtpTime,
+  std::vector<std::string> urls,
+  std::string sessionId
+) {
+}
+
+void RtspHandler::respondPlayAfterPause(std::string sessionId) {
+}
+
+void RtspHandler::respondSwitching(std::string sessionId) {
+}
+
+void RtspHandler::respondCameraChange(std::string sessionId) {
+}
+
+void RtspHandler::respondBitrateChange(std::string sessionId) {
+}
+
+void RtspHandler::respondTeardown() {
+}
+
+void RtspHandler::respondError(int error, std::string rtspMethod) {
+}
+
+void RtspHandler::respondPause(Buffer &buffer) {
+}
+
+std::string RtspHandler::findUserName(std::vector<std::string> strings) {
+}
+
+int RtspHandler::findCSeq(std::vector<std::string> strings) {
+}
+
+std::string RtspHandler::findContents(std::string line0) {
+}
+
+std::string RtspHandler::getMediaInfo(std::string fullCid) {
+}
+
+int RtspHandler::findTrackId(std::string line0) {
+}
+
+std::string RtspHandler::findTransport(std::vector<std::string> strings) {
+}
+
+std::string RtspHandler::findHybridMode(std::vector<std::string> strings) {
+}
+
+std::string RtspHandler::findNotTx(std::vector<std::string> strings) {
+}
+
+std::vector<int> RtspHandler::findChannels(std::string transport) {
+}
+
+std::string RtspHandler::findSessionId(std::vector<std::string> strings) {
+}
+
+std::vector<float> RtspHandler::findNormalPlayTime(std::vector<std::string> strings) {
+}
+
+std::string RtspHandler::findDeviceModelName(std::vector<std::string> strings) {
+}
+
+std::string RtspHandler::findManufacturer(std::vector<std::string> strings) {
+}
+
+int RtspHandler::findLatestReceivecSampleIdx(
+  std::vector<std::string> strings,std::string filter
+) {
+}
+
+bool RtspHandler::isSeekRequest(std::vector<std::string> strings) {
+}
+
+bool RtspHandler::isValidPlayTime(std::vector<float> ntpSec) {
+}
+
+std::string RtspHandler::getContentsTitle(std::vector<std::string> urls) {
+}
+
+std::string RtspHandler::getSupportingBitrateTypes(std::vector<int> bitrateTypes) {
+}
+
+bool RtspHandler::isContainingPlayInfoHeader(std::vector<std::string> strings) {
+}
+
+bool RtspHandler::isThereMonitoringInfoHeader(std::vector<std::string> strings) {
+}
+
+void RtspHandler::parseHybridVideoSampleMetaDataForDandS(std::string notTxIdListStr) {
 }
