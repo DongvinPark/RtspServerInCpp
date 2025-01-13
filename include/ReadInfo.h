25fbed25ad60b9c33dda214ffffe5b4c7e944ece
DongvinPark <dongvin99@naver.com>
2025-01-13 21:46:51 +0900

    added ReadInfo class

---
+++ include/ReadInfo.h
@@ -0,0 +1,38 @@
+#ifndef READINFO_H
+#define READINFO_H
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include "../include/AudioSampleInfo.h"
+#include "../include/VideoAccess.h"
+#include "../constants/C.h"
+
+class ReadInfo {
+public:
+  int startSampleNo = C::INVALID;
+  int endSampleNo = C::INVALID;
+  int curSampleNo = C::INVALID;
+
+  int refSeq0 = C::INVALID;
+  std::vector<int> channel{};
+  int64_t unitTimeUs = C::INVALID_OFFSET;
+  int maxSampleNo = C::INVALID;
+
+  int64_t timestamp = C::INVALID_OFFSET;
+  int unitFrameCount = C::INVALID;
+
+  int curVid = C::INVALID;
+  int curMemberVid = C::INVALID;
+
+  int64_t curPresentationTimeUs = C::INVALID_OFFSET;
+
+  // TODO : AudioSampleInfo and VideoAccess are non-copyable
+  // TODO : Should refactor these fields later.
+  //std::vector<AudioSampleInfo> audioMeta{};
+  //std::unordered_map<std::string, VideoAccess> videoMeta{};
+};
+
+#endif //READINFO_H
