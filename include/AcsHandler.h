9466418704b9cfbc13ecdf2a63bd11323641fef4
DongvinPark <dongvin99@naver.com>
2025-01-14 22:44:13 +0900

    init member function in AcsHandler class

--- include/AcsHandler.h
+++ include/AcsHandler.h
@@ -5,6 +5,8 @@
 #include "../include/Session.h"
 #include "../include/RtpInfo.h"
 #include "../include/ReadInfo.h"
+#include "../include/Buffer.h"
+#include "../include/AVSampleBuffer.h"
 
 class Session;
 class RtspHandler;
@@ -19,7 +21,55 @@ public:
   );
   ~AcsHandler();
 
+  void updateRtpRemoteCnt(int cnt);
+  void updateCurSampleNo(int mediaType, int idx);
+  int getCamId();
+  void shutdown();
+
+  std::weak_ptr<FileReader> getFileReaderPtr();
+
+  void setChannel(int streamId, std::vector<int> ch);
+  void initUserRequestingPlaytime(std::vector<float> timeS);
+  void setRtpInfo(RtpInfo& rtpInfo);
+  void setReader(std::weak_ptr<FileReader> reader);
+  int getLastVideoSampleNumber();
+  int getLastAudioSampleNumber();
+  std::vector<unsigned char> getAccData();
+  std::vector<std::vector<unsigned char>> getAllV0Images();
+  std::unique_ptr<AVSampleBuffer> getNextSample();
+  bool isDone(int streamId);
+  int64_t getUnitFrameTimeUs(int streamId);
+  std::string getMediaInfo();
+  std::vector<int64_t> getSsrc();
+  int getMainVideoNumber();
+  int getMaxCamNumber();
+  std::vector<int> getInitialSeq();
+  std::vector<long> getTimestamp();
+  int64_t getTimestamp0(int streamId);
+  int64_t getUnitFrameCount(int streamId);
+  std::vector<int> getUnitFrameCount();
+  std::vector<std::string> getStreamUrls();
+  void setStreamUrl(int streamId, std::string url);
+  std::vector<int64_t> getPlayTimeUs();
+  int64_t getPlayTimeUs(int streamId);
+  std::vector<int64_t> getGop();
+  void setCamId(int camId);
+  void findNextSampleForSwitching(int vid, std::vector<int64_t> timeInfo);
+
 private:
+  std::unique_ptr<Buffer> get1stRtpOfRefSample(int streamId, int sampleNo);
+  void checkTimestamp(int streamId, ReadInfo& readInfo);
+  int findKeySampleNumber(int streamId, int64_t timeUs, int way);
+  int findSampleNumber(int streamId, int64_t timeUs);
+  int getSampleNumber(int streamId, int64_t timeUs);
+  int findNextSampleForSwitchingAudio(std::vector<int64_t> timeInfo);
+  void findNextSampleForSwitchingVideo(int nextVid, std::vector<int64_t> timeInfo);
+  std::vector<int64_t> getUnitFrameTimeUs();
+  int64_t getSamplePresentationTimeUs(int streamId, int64_t timestamp);
+  int64_t getSamplePresentationTimeUs(int streamId, int sampleTimeIndex);
+  int getSampleTimeIndex(int streamId, int64_t timestamp);
+  int64_t getTimestamp(int sampleNo);
+
   std::shared_ptr<Logger> logger;
   std::string sessionId;
 
--- src/service/AcsHandler.cpp
+++ src/service/AcsHandler.cpp
@@ -7,3 +7,135 @@ AcsHandler::AcsHandler(
 ) {}
 
 AcsHandler::~AcsHandler() {}
+
+void AcsHandler::updateRtpRemoteCnt(int cnt) {
+}
+
+void AcsHandler::updateCurSampleNo(int mediaType, int idx) {
+}
+
+int AcsHandler::getCamId() {
+}
+
+void AcsHandler::shutdown() {
+}
+
+std::weak_ptr<FileReader> AcsHandler::getFileReaderPtr() {
+}
+
+void AcsHandler::setChannel(int streamId, std::vector<int> ch) {
+}
+
+void AcsHandler::initUserRequestingPlaytime(std::vector<float> timeS) {
+}
+
+void AcsHandler::setRtpInfo(RtpInfo &rtpInfo) {
+}
+
+void AcsHandler::setReader(std::weak_ptr<FileReader> reader) {
+}
+
+int AcsHandler::getLastVideoSampleNumber() {
+}
+
+int AcsHandler::getLastAudioSampleNumber() {
+}
+
+std::vector<unsigned char> AcsHandler::getAccData() {
+}
+
+std::vector<std::vector<unsigned char>> AcsHandler::getAllV0Images() {
+}
+
+std::unique_ptr<AVSampleBuffer> AcsHandler::getNextSample() {
+}
+
+bool AcsHandler::isDone(int streamId) {
+}
+
+int64_t AcsHandler::getUnitFrameTimeUs(int streamId) {
+}
+
+std::string AcsHandler::getMediaInfo() {
+}
+
+std::vector<int64_t> AcsHandler::getSsrc() {
+}
+
+int AcsHandler::getMainVideoNumber() {
+}
+
+int AcsHandler::getMaxCamNumber() {
+}
+
+std::vector<int> AcsHandler::getInitialSeq() {
+}
+
+std::vector<long> AcsHandler::getTimestamp() {
+}
+
+int64_t AcsHandler::getTimestamp0(int streamId) {
+}
+
+int64_t AcsHandler::getUnitFrameCount(int streamId) {
+}
+
+std::vector<int> AcsHandler::getUnitFrameCount() {
+}
+
+std::vector<std::string> AcsHandler::getStreamUrls() {
+}
+
+void AcsHandler::setStreamUrl(int streamId, std::string url) {
+}
+
+std::vector<int64_t> AcsHandler::getPlayTimeUs() {
+}
+
+int64_t AcsHandler::getPlayTimeUs(int streamId) {
+}
+
+std::vector<int64_t> AcsHandler::getGop() {
+}
+
+void AcsHandler::setCamId(int camId) {
+}
+
+void AcsHandler::findNextSampleForSwitching(int vid, std::vector<int64_t> timeInfo) {
+}
+
+std::unique_ptr<Buffer> AcsHandler::get1stRtpOfRefSample(int streamId, int sampleNo) {
+}
+
+void AcsHandler::checkTimestamp(int streamId, ReadInfo &readInfo) {
+}
+
+int AcsHandler::findKeySampleNumber(int streamId, int64_t timeUs, int way) {
+}
+
+int AcsHandler::findSampleNumber(int streamId, int64_t timeUs) {
+}
+
+int AcsHandler::getSampleNumber(int streamId, int64_t timeUs) {
+}
+
+int AcsHandler::findNextSampleForSwitchingAudio(std::vector<int64_t> timeInfo) {
+}
+
+void AcsHandler::findNextSampleForSwitchingVideo(int nextVid, std::vector<int64_t> timeInfo) {
+}
+
+std::vector<int64_t> AcsHandler::getUnitFrameTimeUs() {
+}
+
+int64_t AcsHandler::getSamplePresentationTimeUs(int streamId, int64_t timestamp) {
+}
+
+int64_t AcsHandler::getSamplePresentationTimeUs(int streamId, int sampleTimeIndex) {
+}
+
+int AcsHandler::getSampleTimeIndex(int streamId, int64_t timestamp) {
+}
+
+int64_t AcsHandler::getTimestamp(int sampleNo) {
+}
