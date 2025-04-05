da048cd7607daed99b3e3622dfa9788e8d0f46f7
DongvinPark <dongvin99@naver.com>
2025-04-05 11:40:44 +0900

    do not delete session immediately after sending TEARDOWN res

--- src/service/Session.cpp
+++ src/service/Session.cpp
@@ -710,7 +710,7 @@ void Session::asyncReceive() {
         std::cout << "\n";
         transmitRtspRes(std::move(bufferPtr));
         if (isTearRes || isErrorRes) {
-          onTeardown();
+          Util::delayedExecutorAsyncByThread(1000, [this](){onTeardown();});
           return; // stop receiving rtsp req after shutting down session
         }
       }
